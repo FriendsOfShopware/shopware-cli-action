22e7ff009efb03cb4b5acd79b3dfe18a4f96adb0
Soner Sayakci <s.sayakci@shopware.com>
2023-06-21 22:44:34 +0200

    fix: inputs

--- src/context.ts
+++ src/context.ts
@@ -1,12 +1,9 @@
 import { platform, arch } from 'os';
-import { getInput, getBooleanInput } from '@actions/core';
+import { getInput } from '@actions/core';
 
 export const osPlat: string = platform();
 export const osArch: string = arch();
 
 export const inputs = {
   version: getInput('version') || 'latest',
-  args: getInput('args'),
-  cwd: getInput('cwd') || '.',
-  installOnly: getBooleanInput('install-only'),
 };
